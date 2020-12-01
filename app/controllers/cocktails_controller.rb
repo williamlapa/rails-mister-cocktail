60e0a9f45b0630edc57680659525fd6bd32a2e18
william lapa <williamlapa@hotmail.com>
2020-12-01 20:08:09 -0300

    style o melhor que pude

--- app/controllers/cocktails_controller.rb
+++ app/controllers/cocktails_controller.rb
@@ -22,6 +22,12 @@ class CocktailsController < ApplicationController
     end
   end
 
+  def destroy
+    @cocktail = Cocktail.find(params[:id])
+    @cocktail.destroy
+    redirect_to cocktails_url
+  end
+
   private
 
   def cocktail_params
